336dfbacf41d9a5254be46037e213818dac890ab
kaiqi <zhang.kaiqi1@northeastern.edu>
2024-01-16 20:36:16 -0800

    added pagination

--- src/main/java/com/ecommerce/webApp/config/MyDataRestConfig.java
+++ src/main/java/com/ecommerce/webApp/config/MyDataRestConfig.java
@@ -2,14 +2,28 @@ package com.ecommerce.webApp.config;
 
 import com.ecommerce.webApp.entity.Product;
 import com.ecommerce.webApp.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import org.hibernate.type.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+    }
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors){
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
@@ -24,5 +38,31 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // call an internal helper method
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        // expose entity ids
+        config.exposeIdsFor(entityManager.getMetamodel()
+                .getEntities().stream()
+                .map(e -> e.getJavaType()).collect(Collectors.toList())
+                .toArray(new Class[0]));
+
+//        // get a list of all entity classes from the entity manager
+//        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+//
+//        // create an array of the entity types
+//        List<Class> entityClasses = new ArrayList<>();
+//
+//        // get the entity type for the entities
+//        for (EntityType tempEntityType: entities){
+//            entityClasses.add(tempEntityType.getJavaType());
+//        }
+//
+//        // expose the entity ids for the array of entity/domain types
+//        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+//        config.exposeIdsFor(domainTypes);
     }
 }
